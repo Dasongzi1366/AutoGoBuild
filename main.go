354e6a9d15695177c2942827d6cca2b75c015d2f
Dasongzi1366 <13662618@qq.com>
2024-09-13 00:06:10 +0800

    AutoGoBuild

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+//go:embed AutoGo.exe
+var autoGoExe []byte
+
+func main() {
+	goPath := os.Getenv("GOPATH")
+	if goPath == "" {
+		fmt.Println("GOPATH is not set, please set it before running this program.")
+		return
+	}
+
+	binDir := filepath.Join(goPath, "bin")
+	autoGoPath := filepath.Join(binDir, "AutoGo.exe")
+
+	if err := os.MkdirAll(binDir, 0755); err != nil {
+		fmt.Println("Failed to create bin directory:", err)
+		return
+	}
+
+	if err := os.WriteFile(autoGoPath, autoGoExe, 0755); err != nil {
+		fmt.Println("Failed to write AutoGo.exe:", err)
+		return
+	}
+
+	fmt.Println("successfully")
+}
