223e41beeace5f353b7b9cc7c5cc9230f6dc6b46
Dasongzi1366 <13662618@qq.com>
2024-09-13 00:06:10 +0800

    AutoGoBuild

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module app
+module github.com/Dasongzi1366/AutoGoBuild
 
 go 1.23.0
--- main.go
+++ main.go
@@ -11,24 +11,22 @@ import (
 var autoGoExe []byte
 
 func main() {
+	// 获取 GOPATH
 	goPath := os.Getenv("GOPATH")
 	if goPath == "" {
 		fmt.Println("GOPATH is not set, please set it before running this program.")
 		return
 	}
 
+	// 构造目标路径
 	binDir := filepath.Join(goPath, "bin")
 	autoGoPath := filepath.Join(binDir, "AutoGo.exe")
 
-	if err := os.MkdirAll(binDir, 0755); err != nil {
-		fmt.Println("Failed to create bin directory:", err)
-		return
-	}
-
+	// 写入 AutoGo.exe 到 GOPATH/bin
 	if err := os.WriteFile(autoGoPath, autoGoExe, 0755); err != nil {
 		fmt.Println("Failed to write AutoGo.exe:", err)
 		return
 	}
 
-	fmt.Println("successfully")
+	fmt.Println("AutoGo.exe has been successfully installed to", autoGoPath)
 }
